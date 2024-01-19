32d87d253bb3ad3dc6e4cf3d023740302a085c43
Aishu Gyawali <gyawaliaishu@gmail.com>
2024-01-19 19:44:28 +0545

    feat: setup basic homepage

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import Homepage from './pages/Homepage.js'
+import About from './pages/About.js'
+import Footer from './pages/Footer.js'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello World</h1>
+        <div className="cotainer">
+
+            <Homepage/>
+            <About />
+            <Footer />
+        </div>
     </>
   )
 }
---
+++ front-end/src/pages/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+   <>
+    Welcome to samriddhi    
+   </>
+  )
+}
+
+export default Homepage
