fa16bbd502064c2e17aab38170d1c53e830ae0d1
Riya Pant <pantriyapant@gmail.com>
2024-01-19 19:45:33 +0545

    no changes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() {      
 
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
     </>
   )
 }
