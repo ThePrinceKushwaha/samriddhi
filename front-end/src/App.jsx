9ff2065c28410178ed3d23a03e038419b7840e4a
Prince Kushwaha <princekushwaha22@yahoo.com>
2024-01-20 06:37:32 +0545

    update pages

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,18 +3,37 @@ import './App.css'
 import Homepage from './pages/Homepage'
 import About from './pages/About'
 import Footer from './pages/Footer'
+import Signup from './pages/Signup'
+import {Routes, Route} from 'react-router-dom';
+import Login from './pages/Login'
+import Contact from './pages/Contact';
+import Error from './pages/Error';
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-        <div className="container flex bg-purple">
+<Routes>
+  <Route path="/" element= {<Homepage />}></Route>
+  <Route path="/signup" element= {<Signup />}></Route>
+  <Route path="/login" element={<Login />}> </Route>
+  <Route path="/contact" element={<Contact />}></Route>
+  <Route path="/about" element={<About />}></Route>
+  <Route path="/*" element={<Error />}></Route>
+</Routes>
+
+
+    {/* <Signup /> */}
+    {/* <Login /> */}
+        {/* <div className="container flex bg-purple">
 
             <Homepage/>
             <About />
+            <Signup />
             <Footer />
-        </div>
+
+        </div> */}
     </>
   )
 }
---
+++ front-end/src/pages/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div>   
+        Oops!!
+        404 Not Found
+    </div>
+  )
+}
+
+export default Error
